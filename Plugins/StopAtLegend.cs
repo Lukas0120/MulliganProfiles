67396f8fd6cd00838de759c3db6108dab88afdb0
ArthurFairchild <supervalid@mymail.com>
2015-10-14 20:02:41 -0500

    plugin stops whenever you hit legend rank

---
+++ Plugins/StopAtLegend.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SmartBot.Plugins;
+using SmartBot.Plugins.API;
+
+namespace SmartBotPlugins_art
+{
+    [Serializable]
+    public class EndAtLegendContainer : PluginDataContainer
+    {
+
+        //Init vars
+        public EndAtLegendContainer()
+        {
+            Name = "EndAtLegend";
+        }
+    }
+
+    public class GraveyardSaver : Plugin
+    {
+       
+        public override void OnGameEnd()
+        {
+            if (Bot.GetPlayerDatas().GetRank() != 0) return;
+            Bot.Log("You are legend, so pat yourself on the back for... uhm, resilience to convince your real life friends that you are not a bot. ");
+            Bot.StopBot();
+        }
+    }
+}
