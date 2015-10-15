7e651c6e0ab19dead4214e4d7839dd8e34774eb5
ArthurFairchild <supervalid@mymail.com>
2015-10-14 20:23:33 -0500

    limited to ranked mode only

--- Plugins/EndAtLegend.cs
+++ Plugins/EndAtLegend.cs
@@ -17,11 +17,12 @@ namespace SmartBotPlugins_art
 
     public class GraveyardSaver : Plugin
     {
-       
+
         public override void OnGameEnd()
         {
-            if (Bot.GetPlayerDatas().GetRank() != 0) return;
-            Bot.Log("You are legend, so pat yourself on the back for... uhm, resilience to convince your real life friends that you are not a bot. ");
+            if (Bot.CurrentMode() != Bot.Mode.Ranked || Bot.GetPlayerDatas().GetRank() != 0) return;
+            Bot.Log(
+                "You are legend, so pat yourself on the back for... uhm, resilience to convince your real life friends that you are not a bot. ");
             Bot.StopBot();
         }
     }
